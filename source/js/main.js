a58f8dd583eee8e3183ee7355d2d72ea278f8f25
Herman Krasnopolskyi <herma.krasnopolskyi@gmail.com>
2021-05-17 14:39:19 +0300

    Retina, nojs

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,9 @@ const burger = document.querySelector('.main-nav__toggle');
 const header = document.querySelector('.page-header');
 const user = document.querySelector('.user');
 
+header.classList.remove('page-header--nojs');
+user.classList.add('user--title');
+
 burger.addEventListener("click", function(e) {
   header.classList.toggle('page-header--active');
   user.classList.toggle('user--title');
